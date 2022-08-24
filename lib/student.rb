cd71eafc0670b1cae9a8efb04001526555ac818f
k-koech <kelvin.koech@student.moringaschool.com>
2022-08-24 09:59:27 +0300

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative "./user"
 
+class Student < User
+    attr_reader :knowledge
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(myString)
+        @knowledge << myString
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
